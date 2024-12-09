41e7714763fad47397e3dc7bd57169736f7ee045
tenyega <tenyega23@gmail.com>
2024-12-09 15:28:26 +0100

    settings

--- chatbot.js
+++ chatbot.js
@@ -65,45 +65,9 @@ form.addEventListener('submit', (e) => {
     console.log('question', question.value);
 
 
-    // if (question.value === "hello") {
-    //     answer.innerText = responses.hello;
-
-    // } else if (question.value === "how are you") {
-    //     answer.innerText = responses["how are you"];
-
-
-    // } else if (question.value === 'what is your name') {
-    //     answer.innerText =responses["what is your name"]
-
-
-    // }else if (question.value === 'default') {
-    //     answer.innerText = responses["default"];
-
-
-    // }else if (question.value === 'toxic') {
-    //     answer.innerText = responses["toxic"];
-    // }
-
-    console.log('slsl', responses.hasOwnProperty(question.value));
-
     answer.innerHTML = responses.hasOwnProperty(question.value) ? responses[question.value] : responses['default']
 
-    //ANother way of doing by Quentin
-    //answer.innerHTML = responses[question.value] || responses['default']
 
-    // another way by prof
     const inputValue = question.value.toLowerCase().trim();
-    // answer.innerHTML = responses[inputValue] || responses['default'];
 
-    //another way by prof
-    // for (const key in responses) {
-    //     if (inputValue.includes[key]) {
-    //         console.log('key', key);
-    //         answer.innerHTML = responses[key];
-    //         find = true;
-    //     }
-    // }
-    // if (!find) {
-    //     answer.innerHTML = responses.default;
-    // }
 })
